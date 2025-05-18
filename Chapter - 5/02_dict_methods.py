0ad518c5054e04d0f2bb60d962ceb1a9354807d4
Shampy <157746114+codershampy@users.noreply.github.com>
2025-05-18 12:23:17 +0530

    Add files via upload

---
+++ Chapter - 5/02_dict_methods.py
@@ -0,0 +1,15 @@
+d = {} # Empty dictionary
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23
+}
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"Harry": 99, "Renuka":100})
+print(marks)
+
+print(marks.get("Harry2"))#Prints None
+print(marks["Harry2"])#Returns an error
+
