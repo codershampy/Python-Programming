3f64a18a34430a293836faee34508fbe59844985
Shampy <157746114+codershampy@users.noreply.github.com>
2025-05-21 21:52:56 +0530

    Add files via upload

---
+++ Chapter 6 - PS/Problem5.py
@@ -0,0 +1,9 @@
+list =["Harry","Sara","Sanjay","Rajat"]
+ 
+name = input("Enter your name:")
+
+if(name in list):
+        print("Your Name is in list")
+
+else:
+        print("Your name is not in list")
